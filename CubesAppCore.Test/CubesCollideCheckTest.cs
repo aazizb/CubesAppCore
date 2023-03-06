cb0b55d6fb2ea13927bf30c2cc4d0e377a1557ea
aaziz <aazizb@hotmail.com>
2023-03-06 17:05:34 +0100

    No touching cubes collide check passed

--- CubesAppCore.Test/CubesCollideCheckTest.cs
+++ CubesAppCore.Test/CubesCollideCheckTest.cs
@@ -38,5 +38,22 @@ namespace CubesAppCore.Test
             //Assert
             Assert.True(collide);
         }
+        [Fact]
+        public void Cubes_do_touch()
+        {
+            //Arrange
+            Cube cubeA = new CubeBuilder()
+                            .WithCenter(new Point(2, 2, 2))
+                            .WithLength(2)
+                            .Build();
+            Cube cubeB = new CubeBuilder()
+                            .WithCenter(new Point(4, 2, 2))
+                            .WithLength(2)
+                            .Build();
+            //Act
+            bool collide = cubeA.CollidesWith(cubeB);
+            //Assert
+            Assert.True(collide);
+        }
     }
 }
