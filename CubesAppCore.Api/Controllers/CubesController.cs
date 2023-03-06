a2f49b77db1d1917872832cd3718ddb0c3c8384a
aaziz <aazizb@hotmail.com>
2023-03-06 23:03:01 +0100

    Added some comments

---
+++ CubesAppCore.Api/Controllers/CubesController.cs
@@ -0,0 +1,47 @@
+﻿using CubesAppCore.Api.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace CubesAppCore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CubesController : ControllerBase
+    {
+        private readonly ILogger<CubesController> logger;
+
+        public CubesController(ILogger<CubesController> logger)
+        {
+            this.logger = logger;
+        }
+        /// <summary>
+        /// Computes the common volume between two cubes if they cillide
+        /// </summary>
+        /// <param name="requestModel"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult GetCommonVolume([FromBody] CubesRequestModel requestModel)
+        {
+            //this peace of code should go in another class out side the controller
+            try
+            {
+                Cube cubeA = new CubeBuilder()
+                    .WithCenter(requestModel.PointA)
+                    .WithLength(requestModel.LengthA)
+                    .Build();
+                Cube cubeB = new CubeBuilder()
+                   .WithCenter(requestModel.PointB)
+                   .WithLength(requestModel.LengthB)
+                   .Build();
+                double volume = cubeA.CommonVolumeWith(cubeB);
+                return Ok(volume);
+
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message, ex);
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ CubesAppCore.Api/Models/CubesRequestModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CubesAppCore.Api.Models
+{
+    public class CubesRequestModel
+    {
+        public Point PointA { get; set; }
+        public double LengthA { get; set; }
+        public Point PointB { get; set; }
+        public double LengthB { get; set; }
+    }
+}
